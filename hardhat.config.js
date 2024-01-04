d785419f5a680fc469a6f029e64ae53e019755f8
Daniel <88243933+DanielCMartensson@users.noreply.github.com>
2024-01-04 09:16:36 +0100

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 const dotenv = require("dotenv");
 dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  defaultNetwork: "polygon_mumbai",
   networks: {
-    polygon: {
+    polygon_mumbai: {
       url: process.env.ALCHEMY_TESTNET_RPC_URL,
-      accounts: [`0x${process.env.TESTNET_PRIVATEKEY}`],
+      accounts: [process.env.TESTNET_PRIVATEKEY],
     },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
   },
   paths: {
     sources: './contracts',
