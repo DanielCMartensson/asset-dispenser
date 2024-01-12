1a42c9d31d59a1fedfb200c39ff0f6d2fd1456fc
Daniel <88243933+DanielCMartensson@users.noreply.github.com>
2024-01-12 13:01:31 +0100

    additional styling and functionality

--- client/src/components/Minting/Minting.jsx
+++ client/src/components/Minting/Minting.jsx
@@ -9,7 +9,13 @@ const GameNftAddress = "0x180B68507CB960a3314141F174B0dd71b5a8DA97";
 const Minting = () => {
   const [mintAmount, setMintAmount] = useState(1);
   const [accounts, setAccounts] = useState([]);
+  const [isPublicMint, setPublicMint] = useState(false);
+  const [isPublicMintOpen, setPublicMintOpen] = useState();
   const isConnected = Boolean(accounts[0]);
+
+  const handleMintOptions = () => {
+    setPublicMint(!isPublicMint)
+  };
 
   async function connectAccount() {
     if(window.ethereum) {
@@ -30,8 +36,8 @@ const Minting = () => {
         signer
       );
       try {
-        const response = await contract.publicMint(BigNumber.from(mintAmount), {
-          value: ethers.utils.parseEther((0*mintAmount).toString()) 
+        const response = await contract.publicMint("https://ipfs.filebase.io/ipfs/QmWUZXX79cFf93fkWwaNDi6ttrf4f2ZBPkG7DfQ4eV6uYJ", BigNumber.from(mintAmount), {
+          value: ethers.utils.parseEther((0.01*mintAmount).toString()) 
         });
         console.log("response: ", response);
       } catch (err) {
@@ -65,10 +71,19 @@ const Minting = () => {
       )}
         {isConnected ? (
           <div className='minting_options'>
+          {isPublicMint ? (
             <div className='info_container'>
-                <h2> What are we minting?</h2>
-                <p>Your wallet address is eligable to mint the collection</p>
+                <h2> Public minting</h2>
+                <p> If the public minting is open, you can mint...</p>
+                <button className='opt_btn' onClick={handleMintOptions}>To Prepaid Mint</button>
             </div>
+          ) : (
+            <div className='info_container'>
+                <h2> Prepaid minting</h2>
+                <p> If you have been in contact with us and your wallet is prepaid, you can start minting</p>
+                <button className='opt_btn' onClick={handleMintOptions}>To Public Mint</button>
+            </div>
+          )}
             <div>
               <button className='decrement_btn' onClick={handleDecrement}>-</button>
               <input className='amount_input' type='number' readOnly value={mintAmount}></input>
